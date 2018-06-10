d6c85ec85e3d6bd5ceeda06c3e23f699f9a5b1e2
LepkoQQ <lepko.san@gmail.com>
2018-06-10 17:55:45 +0200

    update design

--- script.js
+++ script.js
@@ -1,64 +1,75 @@
 $(function ready() {
-  if ($(window).height() > $('.landing').outerHeight(true) + 50) {
-    $('.scroll-arrow').hide();
+  var petitionName = 'copyleft';
+
+  function showAllSignatures() {
+    $('.js-more').remove();
+    $('.js-signaturenames').removeClass('show-less');
   }
 
-  $(window).on('scroll', function () {
-    $('.scroll-arrow').hide();
+  $('.js-more').on('click', showAllSignatures);
+
+  $.ajax('https://api.djnd.si/getAllSignaturesAndCountForMultiple/?peticije=' + petitionName).done(function(res) {
+    var $sigNames = $('.js-signaturenames');
+    $sigNames.text(res.names);
+    if (parseInt($sigNames.css('max-height'), 10) > $sigNames.height()) {
+      showAllSignatures();
+    }
+    var maxSignatures = parseInt($('.js-signaturemax').text(), 10);
+    var count = res.counter;
+    if (typeof count === 'number' && !isNaN(count)) {
+      var percent = Math.floor(Math.min(count / maxSignatures * 100, 100));
+      $('.js-signaturecount').text(count);
+      $('.petition .progress-bar').attr('aria-valuenow', percent).css('width', percent + '%');
+      $('.petition .progress-bar span').text(percent + '%');
+    }
   });
 
-  // $('.js-signaturenames').load('podpisi.txt', function () {
-  //   var $this = $(this);
-  //   if (parseInt($this.css('max-height'), 10) > $this.height()) {
-  //     showAllSignatures();
-  //   }
-  // });
+  $('.petition__form').on('submit', function(event) {
+    event.preventDefault();
 
-  // function showAllSignatures() {
-  //   $('.js-more').remove();
-  //   $('.js-signaturenames').removeClass('show-less');
-  // }
+    var emailPattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (!$('#petition-email').val().match(emailPattern)) {
+      alert('Tvoj email ne izgleda kot email. Prosim poskusi ponovno.');
+      return;
+    }
 
-  // $('.js-more').on('click', showAllSignatures);
+    var namePattern = /^(([A-Za-zšđžčćäëöüŠĐŽČĆÄËÖÜéÉ]+[\-\']?)*([A-Za-zšđžčćäëöüŠĐŽČĆÄËÖÜéÉ]+)?\s)+([A-Za-zšđžčćäëöüŠĐŽČĆÄËÖÜéÉ]+[\-\']?)*([A-Za-zšđžčćäëöüŠĐŽČĆÄËÖÜéÉ]+)?$/;
+    if (!$('#petition-name').val().match(namePattern)) {
+      alert('Tvoje ime ne izgleda kot ime. Prosim poskusi ponovno.');
+      return;
+    }
 
-  // var maxSignatures = parseInt($('.js-signaturemax').text(), 10);
-  // // $.get('http://djnd-test.lepko.net/podpisek/?k=breakfree&count', function (res) {
-  //   // var count = parseInt(res, 10);
-  //   var count = 5255;
-  //   if (!isNaN(count)) {
-  //     var percent = Math.floor(Math.min(count / maxSignatures * 100, 100));
-  //     $('.js-signaturecount').text(count);
-  //     $('.petition .progress-bar').attr('aria-valuenow', percent).css('width', percent + '%');
-  //     $('.petition .progress-bar span').text(percent + '%');
-  //   }
-  // // });
+    $(this).find(':input').attr('disabled', true);
 
-  // $('.petition__form').on('submit', function (event) {
-  //   event.preventDefault();
-  //   var data = {
-  //     k: 'breakfree',
-  //     name: $('#petition-name').val(),
-  //     email: $('#petition-email').val(),
-  //     petition_greenpeace: $('#petition-greenpeace').val(),
-  //     petition_djnd: $('#petition-djnd').val(),
-  //   };
-  //   console.log(data);
-  //   $.get('http://djnd-test.lepko.net/podpisek/', data, function (res) {
-  //     if (res == 'success') {
-  //       $('.js-petition-error').text('');
-  //       $('.petition__form').hide();
-  //       $('.petition__reset').show();
-  //     } else {
-  //       console.log('error', res);
-  //       $('.js-petition-error').text('Napaka: ' + res);
-  //     }
-  //   });
-  // });
+    var data = {
+      name: $('#petition-name').val(),
+      email: $('#petition-email').val(),
+      peticija: petitionName + '.djnd=' + $('#petition-djnd')[0].checked + '.savethelink=' + $('#petition-savethelink')[0].checked,
+    };
 
-  $('.petition__reset .btn-link').on('click', function () {
+    console.log(data);
+    $('.petition__form').hide();
+    $('.petition__reset').show();
+
+    $.get('https://api.djnd.si/sign/', data, function(res) {
+        if (res === 'Saved') {
+          $('.js-petition-error').text('');
+          $('.petition__form').hide();
+          $('.petition__reset').show();
+        } else {
+          console.error('error', res);
+          $('.js-petition-error').text('Napaka: ' + res);
+        }
+      }
+    );
+  });
+
+  $('.petition__reset .btn-link').on('click', function() {
+    $('.petition__form').find(':input').attr('disabled', false);
+
     $('#petition-name').val('');
     $('#petition-email').val('');
-    $('#petition-greenpeace, #petition-djnd').each(function () {
+    $('#petition-savethelink, #petition-djnd').each(function() {
       this.checked = this.defaultChecked;
     });
 
@@ -73,37 +84,33 @@ $(function ready() {
     data: {
       fatmama: document.location.href,
     },
-    success: function (resp) {
+    success: function(resp) {
       link = resp;
-    }
+    },
   });
 
-  var title = 'Break Free SI';
-  var text = 'Čas je zdaj: osvobodimo se fosilnih goriv!';
-  var hashtags = '#breakfree';
+  var title = '{{title}}';
+  var text = '{{social-text}}';
+  var hashtags = '#{{hashtag}}';
   //social
-  $('.js-facebook').on('click', function () {
+  $('.js-facebook').on('click', function() {
     var url = 'https://www.facebook.com/dialog/feed?app_id=301375193309601&redirect_uri=' + encodeURIComponent(document.location.href) + '&link=' + encodeURIComponent(document.location.href) + '&ref=responsive&name=' + encodeURIComponent(title);
     window.open(url, '_blank');
-    // ga('send', 'event', 'social', 'facebook');
     return false;
   });
-  $('.js-twitter').on('click', function () {
+  $('.js-twitter').on('click', function() {
     var url = 'https://twitter.com/intent/tweet?text=' + encodeURIComponent(text + ' ' + hashtags + ' ' + link);
     window.open(url, '_blank');
-    // ga('send', 'event', 'social', 'twitter');
     return false;
   });
-  $('.js-gplus').on('click', function () {
+  $('.js-gplus').on('click', function() {
     var url = 'https://plus.google.com/share?url=' + encodeURIComponent(document.location.href);
     window.open(url, '_blank');
-    // ga('send', 'event', 'social', 'gplus');
     return false;
   });
-  $('.js-email').on('click', function () {
+  $('.js-email').on('click', function() {
     var url = 'mailto:?subject=' + encodeURIComponent(title) + '&body=' + text + ' ' + encodeURIComponent(document.location.href);
     window.open(url, '_blank');
-    // ga('send', 'event', 'social', 'email');
     return false;
   });
 });
